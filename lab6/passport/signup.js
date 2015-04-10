0930e068f1d1f62a52654f832527b20286d1ca42
Joyee Cheung <joyeec9h3@gmail.com>
2015-04-10 22:06:50 +0800

    more readable

--- lab6/passport/signup.js
+++ lab6/passport/signup.js
@@ -5,55 +5,61 @@ var bCrypt = require('bcrypt-nodejs');
 module.exports = function(passport) {
 
   passport.use('signup', new LocalStrategy({
-      passReqToCallback: true // allows us to pass back the entire request to the callback
-    },
-    function(req, username, password, done) {
+    // allows us to pass back the entire request to the callback
+    passReqToCallback: true
+  }, signup));
 
-      findOrCreateUser = function() {
-        // find a user in Mongo with provided username
-        User.findOne({
-          'username': username
-        }, function(err, user) {
-          // In case of any error, return using the done method
+  function signup(req, username, password, done) {
+
+    function signupWrapper() {
+      // find a user in Mongo with provided username
+      User.findOne({
+        'username': username
+      }, signupHandler);
+    }
+
+    function signupHandler(err, user) {
+      // In case of any error, return using the done method
+      if (err) {
+        console.log('Error in SignUp: ' + err);
+        return done(err);
+      }
+
+      // already exists
+      if (user) {
+        console.log('User already exists with username: ' + username);
+        return done(null, false, req.flash('message', 'User Already Exists'));
+      } else {
+        // if there is no user with that email
+        // create the user
+        var newUser = new User();
+
+        // set the user's local credentials
+        newUser.username = username;
+        newUser.password = createHash(password);
+        newUser.email = req.param('email');
+        newUser.firstName = req.param('firstName');
+        newUser.lastName = req.param('lastName');
+
+        // save the user
+        newUser.save(function(err) {
           if (err) {
-            console.log('Error in SignUp: ' + err);
-            return done(err);
+            console.log('Error in Saving user: ' + err);
+            throw err;
           }
-          // already exists
-          if (user) {
-            console.log('User already exists with username: ' + username);
-            return done(null, false, req.flash('message', 'User Already Exists'));
-          } else {
-            // if there is no user with that email
-            // create the user
-            var newUser = new User();
-
-            // set the user's local credentials
-            newUser.username = username;
-            newUser.password = createHash(password);
-            newUser.email = req.param('email');
-            newUser.firstName = req.param('firstName');
-            newUser.lastName = req.param('lastName');
-
-            // save the user
-            newUser.save(function(err) {
-              if (err) {
-                console.log('Error in Saving user: ' + err);
-                throw err;
-              }
-              console.log('User Registration succesful');
-              return done(null, newUser);
-            });
-          }
+          console.log('User Registration succesful');
+          return done(null, newUser);
         });
-      };
-      // Delay the execution of findOrCreateUser and execute the method
-      // in the next tick of the event loop
-      process.nextTick(findOrCreateUser);
-    }));
+      }
+    }
+
+    // Delay the execution of findOrCreateUser and execute the method
+    // in the next tick of the event loop
+    process.nextTick(signupWrapper);
+  }
 
   // Generates hash using bCrypt
-  var createHash = function(password) {
+  function createHash(password) {
     return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
   }
 
