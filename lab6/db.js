47060d03f3c69ae0cdaa91bdcb1650593d2cb37c
Joyee Cheung <joyeec9h3@gmail.com>
2015-04-12 01:44:31 +0800

    minor changes

--- lab6/db.js
+++ lab6/db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	'url' : 'mongodb://localhost:27017/test'
+	'url' : 'mongodb://localhost:27017/myhomework'
 }
